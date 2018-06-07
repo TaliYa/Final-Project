29ceca4c166f093b6babba54b62909f95d3be553
TaliYa <yazdiy1968@gmail.com>
2018-06-07 13:16:09 +0300

    Update audioAnalysis.py

--- code/audioAnalysis.py
+++ code/audioAnalysis.py
@@ -1,43 +1,44 @@
 from scipy.io import wavfile
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import platform
 import time
 import os
+import cmath
+import random
+from numpy import fft as fft
+import scikits.audiolab as audio
+
 
 constLimit = 500
 arrayPattern = []
 
-sampFreq, data = wavfile.read('./shots1.wav')
-print(data)
-print(data.dtype) #ייצוג המידע מהקובץ
-print(sampFreq) #קצב
-data = data / (2.**15)  # ממפים את הטונים לטווח מסויים
-#print(data)
-#data = data[0:200000]
-#print(data[10000:400000])
-# len(data) - זה מה שמכניסים ב np.arange
+sampFreq, data = wavfile.read('./s-y.wav')
+print(sampFreq) #rate
+#data = data / (2.**15)  # ממפים את הטונים לטווח מסויים
+
 dataLength = len(data)
-print(dataLength)
-#print(data.shape)
-#channel = data[:] # נשתמש בערוץ אחד
-#print(channel)
-'''
+channel = data[:0] # נשתמש בערוץ אחד
+n = len(channel)
+
 timeArray = np.arange(0, dataLength, 1) # 5292 נקודות שיש למידע בקובץ
 timeArray = timeArray / sampFreq
 timeArray = timeArray * 1000  #scale to milliseconds
 
+#fftData = fft.fft(channel)
 
 # שרטוט המידע - דיאגרמה 
-plt.plot(timeArray, data, color='k') 
+#plt.plot(timeArray, data, color='k') 
+plt.plot(fftData, color='k')
 plt.ylabel('Amplitude')
 plt.xlabel('Time (ms)')
 plt.show()
-'''
 
-#function to cheack the the file creation month
+
+
 def check_month(month):
     '''
+    Cheack if the file creation month is normal
     If the month is July or August
     '''
     START_MONTH = "July"
@@ -48,9 +49,10 @@ def check_month(month):
         print("true")
         return True
 
-#function to cheack the the file creation day
+
 def check_day(day):
     '''
+    Cheack if the file creation day is normal
     If the day is Saturday 
     '''
     DAY = "Saturday"
@@ -60,9 +62,11 @@ def check_day(day):
         print("true")
         return True
 
-#function to cheack the the file creation time
+
 def check_time(time):
-    #print(time.replace(":", ""))
+    '''
+    Cheack if the file creation time is normal
+    '''
     START_TIME = "20:00:00"
     END_TIME = "07:00:00"
     
@@ -72,12 +76,14 @@ def check_time(time):
         return False
 
 
-# function to check anomalous audio
-def check_normal(anomalyParams["month"]) and check_day(anomalyParams["day"]) and check_time(anomalyParams["time"]):
+def check_normal(anomalyParams):
+    '''
+    Check anomalous of audio parameters
+    '''
+    if check_normal(anomalyParams["month"]) and check_day(anomalyParams["day"]) and check_time(anomalyParams["time"]):
         return True
             
                
-#function to get the file creation date
 def file_creation_date(path_to_file):
     """
     Try to get the date that a file was created, falling back to when it was
@@ -96,7 +102,9 @@ def file_creation_date(path_to_file):
 
 
 def calc_distances(sound_file):
-    
+    '''
+    Calculate the distances between criticsl points in an audio file
+    '''
     fs, data = wavfile.read(sound_file)
     #print(data)
     data_size = len(data)
@@ -124,47 +132,57 @@ def calc_distances(sound_file):
             idx+=1
     return distances
 
-#comparing the files
-def accept_test(pattren, test, min_error):
+
+def compare_patterns(pattren, test, min_error):
+    '''
+    Compare between 2 patterns off files audio 
+    '''
     if len(pattern) > len(test):
         return False
     for i, dt in enumerate(pattren):
         if not dt - test[i] < min_error:
-            return False
-        
+            return False       
     return True
          
-    
-fileDateCreation = time.ctime(file_creation_date('./shots1.wav'))
+
+
+'''
+P R O C E S S 
+'''
+
+# creat a pattern from the audio file
+pattern = calc_distances('./s-y.wav')    
+
+min_error = 0.1
+for patternItem in arrayPattern:
+    '''
+    checks whether the pattern matches an existing pattern
+    '''
+    if compare_patterns(pattern, patternItem, min_error):
+        print('the pattern match to existing pattren')
+
+'''
+checks anothers parameters of the audio file to check anomaly:
+year, month, day, time...
+'''
+fileDateCreation = time.ctime(file_creation_date('./shots1.wav')) # get the date creation of the audio file
+
 arrayDate = fileDateCreation.split(" ")
 fileCreationDay = arrayDate[0]  #day
 fileCreationMonth = arrayDate[1] #month
 fileCreationYear = arrayDate[5] #year
 fileCreationTime = arrayDate[4] #time
 print(arrayDate)
-#print(fileCreationDay)
-#print(fileCreationYear)
 
-# מערך של פרמטרים לבדיקת אנומאליות הקול
+# array of parameters for check the anomaly of sound
 objAnomalyParams = {"month":fileCreationMonth,
                     "day":fileCreationDay,
                     "time":fileCreationTime}
 
-pattern = calc_distances('./shots1.wav')    
-#arrayPattern.append(pattern)
-min_error = 0.1
-for patternItem in arrayPattern:
-    if accept_test(pattern, patternItem, min_error)):
-        print('buum')
-
-
 if !check_normal(objAnomalyParams):
-    # לולאה שרצה על הערכים ומוצאת את הגבוהים מLIMIT
+    # if the parameters do not meet the ormal audio conditions 
     for audioPoint in data:
        # if audioPoint > constLimit:
             arrayPattern.append(pattern)
             print("bumm")
             break
-
-
-
